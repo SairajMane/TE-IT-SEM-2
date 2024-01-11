1f2242308cf490439ad2293e9b38b5889a0aaf3e
Sairaj Mane <89175730+SairajMane@users.noreply.github.com>
2024-01-11 10:00:27 +0000

    Confirmation Commit

--- WAD/ASG1/ASG1B/js/signup.js
+++ WAD/ASG1/ASG1B/js/signup.js
@@ -1,17 +1,12 @@
-document.querySelector('#submit').addEventListener('click', function () {
+document.querySelector('#submitted').addEventListener('click', function () {
+        event.preventDefault();
         const form = document.getElementById('sign_up');
         const formData = new FormData(form);
         const formObject = {};
-
         formData.forEach(function (value, key) {
             formObject[key] = value;
         });
-
-        // Store the form data in localStorage
         localStorage.setItem('userData', JSON.stringify(formObject));
-
-        // You can also redirect to another page or perform any other actions here
         console.log('Form data stored in localStorage:', formObject);
-
         window.location.href = './signupSuccess.html';
 });
